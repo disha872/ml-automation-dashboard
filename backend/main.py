5b339520e1f790cca648fdb93995a39466454c46
Disha Toshniwal <dishatoshniwal4@gmail.com>
2026-03-28 23:57:04 +0530

    comma

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,10 @@ import os
 # ------------------ DB CONNECTION FUNCTION ------------------
 def get_db_connection():
     return mysql.connector.connect(
-        host = os.getenv("DB_HOST")
-        user = os.getenv("DB_USER")
-        password = os.getenv("DB_PASSWORD")
-        database = os.getenv("DB_NAME"),
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        database = os.getenv("DB_NAME")
     )
 
 
